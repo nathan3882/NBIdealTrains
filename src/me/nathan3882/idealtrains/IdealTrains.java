d16549e7991387749dc48cb5b8375b99a2538ab2
nathan3882 <nathan.allanson14@hotmail.com>
2018-10-25 14:58:39 +0100

    Init

--- src/me/nathan3882/idealtrains/IdealTrains.java
+++ src/me/nathan3882/idealtrains/IdealTrains.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package idealtrains;
+package me.nathan3882.idealtrains;
 
 import com.thalesgroup.rtti._2017_10_01.ldbsv.types.ArrayOfCoaches;
 
