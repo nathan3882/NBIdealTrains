f2ca3306477dcb2b18919a9172b74f07ee2f515a
nathan3882 <nathan.allanson14@hotmail.com>
2018-10-25 15:19:31 +0100

    Added CRS Information

--- src/me/nathan3882/idealtrains/IdealTrains.java
+++ src/me/nathan3882/idealtrains/IdealTrains.java
@@ -6,6 +6,9 @@
 package me.nathan3882.idealtrains;
 
 import com.thalesgroup.rtti._2017_10_01.ldbsv.types.ArrayOfCoaches;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -13,11 +16,18 @@ import com.thalesgroup.rtti._2017_10_01.ldbsv.types.ArrayOfCoaches;
  */
 public class IdealTrains {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+    public static final String STAFF_WEBSERVICE_URL = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/wsdl.aspx?ver=2017-10-01";
+    public static final String STAFF_WEBSERVICE_ENDPOINT = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/ldbsv12.asmx";
+    public static final String AUTHENTICATION_TOKEN = "";
+
+    public static final String CRS_CODE_BOURNEMOUTH = "BMH";
+    public static final String CRS_CODE_POOLE = "POO";
+    public static final String CRS_CODE_BROCKENHURST = "BCU";
+
+    public static List<String> crsCodes;
+
+    public static void main(String args[]) {
+        crsCodes = new ArrayList<>(Arrays.asList(CRS_CODE_BOURNEMOUTH, CRS_CODE_BROCKENHURST));
     
+    }
 }
