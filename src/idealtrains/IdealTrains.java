070b8458b099613ddfd48db7987920be155eda04
nathan3882 <nathan.allanson14@hotmail.com>
2018-10-25 14:50:26 +0100

    Init

---
+++ src/idealtrains/IdealTrains.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idealtrains;
+
+import com.thalesgroup.rtti._2017_10_01.ldbsv.types.ArrayOfCoaches;
+
+/**
+ *
+ * @author natha
+ */
+public class IdealTrains {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
