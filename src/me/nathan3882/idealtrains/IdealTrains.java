a88c16011e08bc7a5728623f8aef908c90359d8e
nathan3882 <nathan.allanson14@hotmail.com>
2018-11-02 14:59:57 +0000

    Constructed valid soap request

--- src/me/nathan3882/idealtrains/IdealTrains.java
+++ src/me/nathan3882/idealtrains/IdealTrains.java
@@ -20,6 +20,8 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -29,15 +31,24 @@ import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFactory;
 import javax.xml.soap.SOAPHeader;
 import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import org.w3c.dom.Document;
 
 /**
  *
@@ -48,7 +59,7 @@ public class IdealTrains {
 
     public static final String STAFF_WEBSERVICE_URL = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/wsdl.aspx?ver=2017-10-01";
     public static final String STAFF_WEBSERVICE_ENDPOINT = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/ldbsv12.asmx";
-    public static final String AUTHENTICATION_TOKEN = "ed126cc4-bfec-4a9e-9686-0d41823c6399";
+    public static final String AUTHENTICATION_TOKEN = "";
 
     public static final String CRS_CODE_BOURNEMOUTH = "BMH";
     public static final String CRS_CODE_POOLE = "POO";
@@ -73,44 +84,70 @@ public class IdealTrains {
         params.setGetNonPassengerServices(false);
         params.setServices("P");
         params.setTimeWindow(140);
+
         JAXBElement<GetBoardByCRSParams> element = fac.createGetDepartureBoardByCRSRequest(params);
 
-        System.out.println("Element QName = " + element.getName());
-        System.out.println("Element declared type " + element.getDeclaredType());
-        System.out.println("Element value = " + element.getValue());
-        System.out.println("Element scope = " + element.getScope());
-
         JAXBContext jaxbContext = JAXBContext.newInstance(GetBoardByCRSParams.class);
+
         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
         // output pretty printed
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-        jaxbMarshaller.marshal(element, System.out);
+        Document document = null;
+        try {
+            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(IdealTrains.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        jaxbMarshaller.marshal(element, document);
 
-        /*try {
-            SOAPMessage soapMessage = null;
+        try {
+            MessageFactory messageFactory = MessageFactory.newInstance();
+            SOAPMessage soapMessage = messageFactory.createMessage();
 
             SOAPEnvelope sen = soapMessage.getSOAPPart().getEnvelope();
-
             sen.addNamespaceDeclaration("ns2", "http://thalesgroup.com/RTTI/2013-11-28/Token/types");
             SOAPHeader sh = sen.getHeader();
             if (sh == null) {
                 sh = sen.addHeader();
             }
-
-            SOAPHeaderElement she = sh.addHeaderElement(
+            SOAPHeaderElement tokenParentElement = sh.addHeaderElement(
                     new QName("http://thalesgroup.com/RTTI/2013-11-28/Token/types", "AccessToken", "ns2"));
 
-            SOAPElement se = she.addChildElement(new QName("http://thalesgroup.com/RTTI/2013-11-28/Token/types", "TokenValue", "ns2"));
+            SOAPElement tokenChildElement = tokenParentElement.addChildElement(
+                    new QName("http://thalesgroup.com/RTTI/2013-11-28/Token/types", "TokenValue", "ns2"));
 
-            se.addTextNode(getTokenValue());
+            tokenChildElement.addTextNode(getTokenValue());
+
+            soapMessage.getSOAPBody().addDocument(document); //adding the generated request by jax client to the body
+
             soapMessage.saveChanges();
+          /*<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns2="http://thalesgroup.com/RTTI/2013-11-28/Token/types">
+              <SOAP-ENV:Header>
+               <ns2:AccessToken>
+                <ns2:TokenValue>XXXXX</ns2:TokenValue>
+               </ns2:AccessToken>
+              </SOAP-ENV:Header>
+               <SOAP-ENV:Body>
+                <GetDepartureBoardByCRSRequest xmlns="http://thalesgroup.com/RTTI/2017-10-01/ldbsv/" xmlns:ns2="http://thalesgroup.com/RTTI/2012-01-13/ldbsv/types" xmlns:ns3="http://thalesgroup.com/RTTI/2016-02-16/ldbsv/types" xmlns:ns4="http://thalesgroup.com/RTTI/2014-02-20/ldbsv/types" xmlns:ns5="http://thalesgroup.com/RTTI/2017-10-01/ldbsv/types" xmlns:ns6="http://thalesgroup.com/RTTI/2015-11-27/ldbsv/types">
+                     <numRows>499</numRows>
+                     <crs>BMH</crs>
+                     <time>1901-03-05T00:00:00.000Z</time>
+                     <timeWindow>1440</timeWindow>
+                     <filterType>to</filterType>
+                     <services>P</services>
+                     <getNonPassengerServices>false</getNonPassengerServices>
+                    </GetDepartureBoardByCRSRequest>
+               </SOAP-ENV:Body>
+            </SOAP-ENV:Envelope> */
+            String endPoint = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/ldbsv12.asmx";
+         
             soapMessage.writeTo(System.out);
 
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
     private static String getTokenValue() {
